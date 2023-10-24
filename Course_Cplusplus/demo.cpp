0ad0dbc0cb9827a409599f87920c152c0a5d3562
Tanmay <tanmayghodecr7@gmail.com>
2023-10-24 13:48:16 +0530

    second commit

--- Course_Cplusplus/demo.cpp
+++ Course_Cplusplus/demo.cpp
@@ -1,7 +1,21 @@
 
 #include <iostream>
+#include <cstdlib>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello World!";
+    int n;
+    cin >> n ;
+    cout << n << "\n";
+    if (n> 4)
+    {
+ cout<< "n greater than 4";
+    }
+   
+    else
+    {
+cout << "n is not greater than 4";
+    }
     return 0;
 }
